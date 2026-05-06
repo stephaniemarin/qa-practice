db0fe2686b53da9123f5d8c64f79e95f5311137f
stephanie <stephanienia05@gmail.com>
2026-05-06 12:16:02 -0400

    Add Selenium automation for SauceDemo login

---
+++ automation/tests/test_login.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_valid_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    
+    assert"inventory.html" in driver.current_url
+    
+    driver.quit()
