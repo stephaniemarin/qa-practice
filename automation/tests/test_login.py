44003f7d45f69eb4ee21ad69fc30ebbae0d34958
stephanie <stephanienia05@gmail.com>
2026-05-06 14:01:14 -0400

    updating edge cases for empty, whitespace, long input, and  special chars

--- automation/tests/test_login.py
+++ automation/tests/test_login.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+#create driver
 def create_driver():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
     return driver
 
+#valid login
 def test_valid_login():
     driver = create_driver()
     
@@ -16,7 +18,8 @@ def test_valid_login():
     assert"inventory.html" in driver.current_url
     
     driver.quit()
-    
+
+#invalid password
 def test_invalid_password_login():
     driver = create_driver()
     
@@ -29,7 +32,8 @@ def test_invalid_password_login():
     assert "Username and password do not match" in error_message
     
     driver.quit()
-    
+
+#locked out user login
 def test_locked_out_user_login():
     driver = create_driver()
     
@@ -42,7 +46,8 @@ def test_locked_out_user_login():
     assert "locked out" in error_message
     
     driver.quit()
-    
+
+#empty username and password
 def test_empty_username_and_password():
     driver = create_driver()
     
@@ -53,4 +58,47 @@ def test_empty_username_and_password():
     assert "Username is required" in error_message
     
     driver.quit()
+
+#whitespace username
+def test_whitespace_username():
+    driver = create_driver()
     
+    driver.find_element(By.ID, "user-name").send_keys(" ")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    
+    error_message = driver.find_element(By.CSS_SELECTOR,"[data-test='error']").text
+    
+    assert "Username is required" in error_message
+    
+    driver.quit()
+    
+#long username
+def test_long_username():
+    driver = create_driver()
+    
+    long_username = "a" * 300
+    
+    driver.find_element(By.ID, "user-name").send_keys(long_username)
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    
+    error_message = driver.find_element(By.CSS_SELECTOR,"[data-test='error']").text
+    
+    assert "Username and password do not match" in error_message
+    
+    driver.quit()
+    
+#special characters
+def test_special_characters_username():
+    driver = create_driver()
+    
+    driver.find_element(By.ID, "user-name").send_keys("@@@###")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    
+    error_message = driver.find_element(By.CSS_SELECTOR,"[data-test='error']").text
+    
+    assert "Username and password do not match" in error_message
+    
+    driver.quit()
